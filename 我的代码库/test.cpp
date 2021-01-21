3a288746978f8a1761586a9bd4d25feb6566aff1
Qp1117b <31130664+Qp1117b@users.noreply.github.com>
2021-01-21 21:52:25 +0800

    修改

--- 我的代码库/test.cpp
+++ 我的代码库/test.cpp
@@ -7,7 +7,6 @@ int main()
 {
     vector<int> a = {1,1,1,2,2,2,3,3,3,4,4,4,2,4,0,0,0,0,0,0,111};
     vector<pair<int,int>> b = my_lib_func_eic(a,eic_order_item.COUNT_UP);
-    cout << "²¢";
 
     for (auto & c : b)
     cout << c.first << " has " << c.second << endl;
