7fdb36802e640c16c80fb42d03e8c11193f54749
Qp1117b <31130664+Qp1117b@users.noreply.github.com>
2021-01-21 20:53:21 +0800

    12

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main()
 {
     vector<int> a = {1,1,1,2,2,2,3,3,3,4,4,4,2,4,0,0,0,0,0,0,111};
     vector<pair<int,int>> b = my_lib_func_eic(a,eic_order_item.NUM_DOWN);
-    cout << "a";
+    cout << "²¢";
 
     for (auto & c : b)
     cout << c.first << " has " << c.second << endl;
